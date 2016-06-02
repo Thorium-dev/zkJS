30f13c498d9173287e71a7c4963f3d6cd845f018
Franck DIOMANDE <fkdiomande@gmail.com>
2016-06-02 07:46:40 +0200

    Ajout de mocha pour les tests unitaires

---
+++ test/demoSpec.js
@@ -0,0 +1,12 @@
+var assert = require("assert");
+
+describe("Test mocha", function () {
+
+
+    it("should do something", function () {
+        var a = 3;
+        assert.equal(a, 5)
+    })
+
+
+});
