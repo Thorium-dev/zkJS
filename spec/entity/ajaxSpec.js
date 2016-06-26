4d93a30ba9b6dedc1e55091bab598425ae3a2edb
Franck DIOMANDE <fkdiomande@gmail.com>
2016-06-26 11:05:12 +0200

    ajaxEntity : Modification des titres des tests unitaires

--- spec/entity/ajaxSpec.js
+++ spec/entity/ajaxSpec.js
@@ -1,7 +1,7 @@
 describe("Unit test for ajaxEntity", function() {
 
     describe("Test for node response", function () {
-        it("Response should be a node type", function() {
+        it("Should response be a node", function() {
             var box = zk().toolbox;
             zk("Ajax")
                 .url("src/entity/ajax.php")
@@ -11,7 +11,7 @@ describe("Unit test for ajaxEntity", function() {
                 }).send();
 
         });
-        it("Response should be a node type (Shortcut)", function() {
+        it("Should response be a node (Shortcut)", function() {
             var box = zk().toolbox;
             zk("Ajax").send("src/entity/ajax.php $post $node", function () {
                 chaiExpect(box.is(this.response)).eql("node")
