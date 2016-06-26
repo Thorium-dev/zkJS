d087f1496b47cda3c4f714695c30df7027519ad4
Franck DIOMANDE <fkdiomande@gmail.com>
2016-06-26 11:28:37 +0200

    style pour le test unitaire

--- spec/index.php
+++ spec/index.php
@@ -4,6 +4,16 @@
     <title>Unit test for zkJS</title>
     <link rel="stylesheet" href="lib/mocha.css"/>
 
+    <style>
+        .suite {
+            width: 80%;
+        }
+        .replay {
+            background-color: #2f6fad !important;
+            color: #000066 !important;
+        }
+    </style>
+
 </head>
 <body>
 
@@ -11,7 +21,6 @@
 
     <script src="../dev/core/zk.js"></script>
     <script src="../dev/entity/nodeEntity.js"></script>
-    <script src="../dev/entity/ajaxEntity.js"></script>
 
     <script src="lib/mocha.js"></script>
     <script src="lib/chai.js"></script>
