787e6f17a01f88f9f26d93794d6489e5b8ad271b
Franck DIOMANDE <fkdiomande@gmail.com>
2016-07-13 07:36:04 +0200

    dateTimePickerEntity : Ajout de la fonction container

--- dev/ui/dateTimePickerEntity.js
+++ dev/ui/dateTimePickerEntity.js
@@ -1,7 +1,21 @@
+zk().register(function DATETIMEPICKER($this){
+    var $self = this, $box = $this.toolbox;
+    zk().toolbox.each($this, function () { $self[this.k] = this.v });
 
-zk().register(function Audio($this){
-    var self = this; zk().toolbox.each($this, function () { self[this.k] = this.v });
+    var $container = null;
 
+    /**
+     * Permet de définir son conteneur.
+     *
+     * @method container
+     * @param {*} selector Sélecteur pour l'objet Node.
+     * @return {DATETIMEPICKER}
+     * @since 1.0
+     */
+    this.container = function (selector) {
+        $container = zk().get("Node").set(selector).class("zk-datetime-container");
+        return $self;
+    };
 
 
 }, {}, {});
--- index.php
+++ index.php
@@ -2,47 +2,21 @@
 <html>
 <head>
     <title>zkJS dev version</title>
-    <link rel="stylesheet" href="spec/lib/mocha.css"/>
+    <link rel="stylesheet" href="styles/stylesheets/datetimepicker.css"/>
     <style>
-        ul.days {
-            display: inline-block;
-            border: 1px solid red;
-            vertical-align: top;
-        }
-        #days{
-            padding-top: 20px;
-            width: 300px;
-        }
-        #days1{
-            padding-top: 100px;
-        }
+
     </style>
 </head>
 <body>
 
-    <ul class="days" id="days" style="font-size: 20px;">
-        <li class="day" id="day-1">Monday</li>
-        <li class="day" id="day-2">Tuesday</li>
-        <li class="day" id="day-3">Wednesday</li>
-        <li class="day" id="day-4">Thursday</li>
-        <li class="day" id="day-5">Friday</li>
-        <li class="day" id="day-6">Saturday</li>
-        <li class="day" id="day-7">Sunday</li>
-    </ul>
+    <div class="date-container">
 
-    <select name="pays" id="pays" multiple>
-        <option value="France">France</option>
-        <option value="Japon">Japon</option>
-        <option value="Italie">Italie</option>
-    </select>
 
-    <form action="ajax.php" name="form1" id="res">
-        <input type="text" class="a" data-zk-assert="alpha" data-zk-message="Valeur non valid !" data-zk-view="#error" value="">
-        <div id="error"></div>
-        <input type="text" class="a" data-zk-assert="beta" data-zk-message="Valeur non valid !" data-zk-view="#error2" value="">
-        <div id="error2"></div>
-        <input type="submit" value="envoyer" id="submit" />
-    </form>
+
+
+    </div>
+
+    <button>Slide</button>
 
 
     <script src="dev/core/zk.js"></script>
@@ -53,16 +27,11 @@
     <script src="dev/entity/routerEntity.js"></script>
     <script src="dev/entity/formEntity.js"></script>
     <script src="dev/entity/validatorEntity.js"></script>
+    <script src="dev/ui/dateTimePickerEntity.js"></script>
 
     <script>
 
-        $("#submit").click(function () {
-
-        });
-
-
-        zk("Form");
-
+        zk("datetimepicker").container(".date-container");
 
 
     </script>
