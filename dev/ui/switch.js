9df337565a951dd1e1f64a158d821c1618e174c7
Thorium-dev <fkdiomande@gmail.com>
2016-07-18 08:33:48 +0200

    switchEntity : Ajout des fichiers de base

---
+++ dev/ui/switch.js
@@ -0,0 +1,12 @@
+zk().register(function SWITCH($this){
+    var $self = this, $box = $this.toolbox, $entity = $this.entity.get;
+    $box.each($this, function () { $self[this.k] = this.v });
+
+    function insertNodeBefore(nouvEl, beforeEl) {
+        var parent = beforeEl.parentNode;
+        parent.insertBefore(nouvEl, beforeEl);
+        return parent
+    }
+
+
+}, {}, {});
